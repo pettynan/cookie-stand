079a6025f5ae3f9fa461c02e8ed207116194c3c2
Peter Tynan <pettynan@amazon.com>
2019-04-01 13:44:08 -0700

    got js and html hooked up, displaying hours.

--- js/app.js
+++ js/app.js
@@ -1,17 +1,35 @@
-// First I need to create an object for each store. 
+// First I need to create an object for each store.
 
 var hoursArray = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm']; // Array that stores each of the time blocks
 
 
-var store1 = {
-  storeLocation:'1st and Pike',
-  minCustomers_pHour: 23,
-  maxCustomers_pHour: 65,
-  avgCookies_pSale: 6.3,
+// var store1 = {
+//   storeLocation:'1st and Pike',
+//   minCustomers_pHour: 23,
+//   maxCustomers_pHour: 65,
+//   avgCookies_pSale: 6.3,
+
+//   randomCustomers: function() {
+//     return Math.random(this.minCustomers_pHour, (this.maxCustomers_pHour) + 1);
+//   }
+
+// };
 
 
-  randomCustomers: function() {
-    return Math.random(this.minCustomers_pHour, (this.maxCustomers_pHour) + 1);
 
-  }
-};
+
+
+// Going to try and create a for loop to actually populate the list on the webpage
+
+
+var ulEl1 = document.getElementById('store1');
+
+
+for (var i = 0 ; i < hoursArray.length ; i++) {
+
+  var liEl = document.createElement('li');
+
+  liEl.textContent = hoursArray[i] + ': ';
+
+  ulEl1.appendChild(liEl);
+}
